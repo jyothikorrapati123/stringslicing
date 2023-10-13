a9c98ee102532e479c01caca104e9e26f0880f58
jyothikorrapati123 <144519938+jyothikorrapati123@users.noreply.github.com>
2023-10-13 15:51:56 +0530

    Add files via upload

---
+++ reversestring.py
@@ -0,0 +1,10 @@
+#revers a string
+a = "Python"[::-1]
+print(a)
+#revers a list items
+a=list(("Jyothi","sumathi","venky"))[::-1]
+print(a)
+#reverce astring without slicing
+a="Jyothi"
+a="".join(reversed(a))
+print(a)
---
+++ stringslice.py
@@ -0,0 +1,8 @@
+name="Jyothi"
+print(name[0:4])
+
+print(name[:])
+print(name[0:])
+print(name[:5])
+print(name[1:])
+print(name[2:])
